d29ee3945dbf97be1675864f969100bfec968b76
Shivam-peace <shivamkr.singh9899@gmail.com>
2025-10-09 18:51:06 +0530

    Create ASCII-CODE.c

---
+++ ASCII-CODE.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    char a;
+    printf("Enter the alphabet for ASCII CODE : ");
+    scanf("%c",&a);
+
+    printf("ASCII CODE FOR THE ALPHABET %C IS %D...",a,a);
+    return 0;
+}
